03b66e4f4e16e0024da9a76ff50e825f04a9df41
Jessica <jessiejessica2322@gmail.com>
2019-11-12 08:45:24 -0600

    Initial Commit

---
+++ ItsNowChecked/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ItsNowChecked
+//
+//  Created by A.M. Student on 11/12/19.
+//  Copyright © 2019 JessicaMontes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
